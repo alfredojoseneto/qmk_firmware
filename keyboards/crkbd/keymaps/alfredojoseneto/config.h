28bae0bcd0160d5a8c91d356b89014ee46353839
Alfredo Neto <alfredojoseneto@gmail.com>
2024-05-21 19:15:51 -0300

    increase the tapping_term to 200

--- keyboards/crkbd/keymaps/alfredojoseneto/config.h
+++ keyboards/crkbd/keymaps/alfredojoseneto/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define TAPPING_TERM 100
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
